cc926e2741981837b090a5e5781b9741c1cfe3fc
Your Name <your-email@example.com>
2025-03-14 00:26:21 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+// // Get references to the navbar links and the corresponding sections
+// const homeLink = document.getElementById("home-link");
+// const aboutLink = document.getElementById("about-link");
+// const servicesLink = document.getElementById("services-link");
+// const contactLink = document.getElementById("contact-link");
+// const testimonailslink = document.getElementById("testimonails-link");
+
+
+// const homeSection = document.getElementById("navbar");
+// const aboutSection = document.getElementById("aboutus");
+// const servicesSection = document.getElementById("services");
+// const testimonailsSection = document.getElementById("testimonails");
+// const contactSection = document.getElementById("footer");
+
+// // Function to scroll to a section when a link is clicked
+// function scrollToSection(section) {
+//     section.scrollIntoView({ behavior: "smooth" });
+// }
+
+// // Add click event listeners to the navbar links
+// homeLink.addEventListener("click", () => {
+//     scrollToSection(homeSection);
+// });
+
+// aboutLink.addEventListener("click", () => {
+//     scrollToSection(aboutSection);
+// });
+
+// servicesLink.addEventListener("click", () => {
+//     scrollToSection(servicesSection);
+// });
+
+
+// testimonailslink.addEventListener("click", () => {
+//     scrollToSection(testimonailsSection);
+// });
+
+// contactLink.addEventListener("click", () => {
+//     scrollToSection(contactSection);
+// });
